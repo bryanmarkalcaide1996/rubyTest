d20a628af456f64dfe06275f9a01a9bbd74ba42f
bryan alcaide <bryanalcaide1996@gmail.com>
2022-06-04 19:57:49 +0800

    Initial Commit to remote repository

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-root "navigate#home"
-get  '/about',    to:  "navigate#about"
-get  '/contact',  to:  "navigate#contact"
+  root "navigate#home"
+  get  '/about',    to:  "navigate#about"
+  get  '/contact',  to:  "navigate#contact"
+  get  '/pricing',  to:  "pricing#pricing"
+  get  '/login'  ,  to:  "login#login"
   # Defines the root path route ("/")
   # root "articles#index"
 end
